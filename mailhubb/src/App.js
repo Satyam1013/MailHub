cf48af90467b3c3d154523b8f224a5f3c2d1dfa1
Satyam Banwale <satyambanwale786@gmail.com>
2023-03-02 14:36:05 +0530

    mailhub project done

---
+++ mailhubb/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import AllRoutes from './Pages/AllRouter';
+function App() {
+  return (
+    <div className="App">
+    <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
